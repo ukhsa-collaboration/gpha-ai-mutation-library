f839551ad9229387cf8d7530a45cb4c4fb6015b3
nfellaby <nfellaby@users.noreply.github.com>
2025-12-16 13:32:20 +0000

    feat: testing - find_schema_for_file

--- tests/test_validate_and_update.py
+++ tests/test_validate_and_update.py
@@ -1,6 +1,10 @@
 import pytest
 from pathlib import Path
 from mutation_table_updater import validate_and_update as vau
+
+test_ha_correct_path = 'tests/tables/ha_correct_test.tsv'
+
+
 @pytest.fixture
 def correct_ha_tsv():
     SCRIPT_DIR = Path(__file__).resolve().parent
@@ -14,7 +18,7 @@ def schemas():
     schemas = vau.load_schemas(schemas_fp)
     return schemas
 
-def test_find_schema_for_file(correct_ha_tsv, schemas):
+def test_find_schema_for_file(schemas):
     ''' Test reading of tsv files '''
-    schema = vau.find_schema_for_file(str(correct_ha_tsv), schemas)
+    schema = vau.find_schema_for_file(str(test_ha_correct_path), schemas)
     print(schema)
