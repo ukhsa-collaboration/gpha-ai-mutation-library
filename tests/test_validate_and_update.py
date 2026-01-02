9e6f1f7ccf07f16b36a636b865fec83447263683
nfellaby <nfellaby@users.noreply.github.com>
2026-01-02 13:49:37 +0000

    feat: added test for correct schema identification

--- tests/test_validate_and_update.py
+++ tests/test_validate_and_update.py
@@ -82,7 +82,7 @@ def test_check_correct_filename(correct_ha_tsv):
 def test_find_schema_for_file(load_schemas, correct_ha_tsv):
     """ Test the correct schema is found for a file """
     sch  = vau.find_schema_for_file(load_schemas, correct_ha_tsv)
-    assert next(iter(sch)) is str('ha') # check first key is 'ha'
+    assert next(iter(sch.items())) is str('ha') # check first key is 'ha'
 
 
 # Test dataframe validation
