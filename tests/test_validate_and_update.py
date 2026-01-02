a9a505037dfa02ce6fd13729b5debd6ec5f2cd2a
nfellaby <nfellaby@users.noreply.github.com>
2026-01-02 13:39:01 +0000

    feat: added test for correct schema identification

--- tests/test_validate_and_update.py
+++ tests/test_validate_and_update.py
@@ -79,9 +79,9 @@ def test_check_correct_filename(correct_ha_tsv):
 
     assert check_fn_return is True
 
-def test_find_schema_for_file(load_schemas, fp):
+def test_find_schema_for_file(load_schemas, correct_ha_tsv):
     """ Test the correct schema is found for a file """
-    sch  = vau.find_schema_for_file(load_schemas, fp)
+    sch  = vau.find_schema_for_file(load_schemas, correct_ha_tsv)
     assert sch is str('ha_avian_influenza_mutation_table_gpha.yml')
 
 
