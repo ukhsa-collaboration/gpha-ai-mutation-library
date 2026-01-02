097173c2d23cb36f285dc5f8ff15de0cc1358a33
nfellaby <nfellaby@users.noreply.github.com>
2026-01-02 15:42:09 +0000

    feat: adding function for validating schemas

--- src/mutation_table_updater/validate_and_update.py
+++ src/mutation_table_updater/validate_and_update.py
@@ -139,7 +139,8 @@ def validate_schemas(schemas: Dict[str, dict]) -> None:
             logging.warning('Segment file %s was missing the essential keys %s. YAML should contain the following primary keys: %s',
                 segment, ", ".join(missing), ", ".join(expected_yaml_main_keys))
             return False
-        
+        else:
+            return True
         # 
         #     assert key in seg_dict, logging.warning(
         #         'Segment file missing expected key %s. Should contain the following primary keys: %s',
