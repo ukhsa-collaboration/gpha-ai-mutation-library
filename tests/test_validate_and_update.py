73c2f21cc3e3c97298b373bf7c5bac421e2915ff
nfellaby <nfellaby@users.noreply.github.com>
2026-01-02 11:11:15 +0000

    feat: added test for correct filename

--- tests/test_validate_and_update.py
+++ tests/test_validate_and_update.py
@@ -46,7 +46,7 @@ def test_validate_dataframe(correct_ha_df, correct_ha_schema):
     errs = vau.validate_dataframe(correct_ha_df, correct_ha_schema)
     assert errs == []
 
-def test_check_filename(failed_fn_tsv, caplog):
+def test_check_incorrect_filename(failed_fn_tsv, caplog):
     """ Test to check incorrect filename are handled correctly"""
     # Capture warnings
     
@@ -69,7 +69,7 @@ def test_check_filename(failed_fn_tsv, caplog):
 
     assert check_fn_return is False
 
-def test_check_filename(correct_ha_tsv):
+def test_check_correct_filename(correct_ha_tsv):
     """ Test to check correct filename are handled correctly"""
     # Capture warnings
     
