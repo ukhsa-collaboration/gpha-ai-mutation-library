ff0d7fd2084a276cb1e27e8b6ab6da49b4bd59b3
nfellaby <nfellaby@users.noreply.github.com>
2026-01-20 16:21:06 +0000

    feat: testing - validate_schemas checking primary keys are present in columns['name']

--- src/mutation_table_updater/validate_and_update.py
+++ src/mutation_table_updater/validate_and_update.py
@@ -145,9 +145,9 @@ def validate_schemas(schemas: Dict[str, dict]) -> None:
             if len(primary_key_dif) > 0:
                 logging.critical('Segment schema file %s had differences between the following primary keys "%s" and column names "%s". Please make sure these are both the same.',
                 segment, ", ".join(missing), ", ".join(expected_yaml_main_keys))
-            print(primary_keys_list)
-            print(column_names)
-            print(primary_key_dif)
+            print(f'primary_keys_list: {primary_keys_list}')
+            print(f'column_names: {column_names}')
+            print(f'primary_key_dif: {primary_key_dif}')
             if len(primary_key_dif) > 0:
                 logging.critical('Segment schema file %s was missing the following essential keys "%s". YAML should contain these essential keys: %s',
                     segment, ", ".join(primary_key_dif), ", ".join(primary_keys_list))
