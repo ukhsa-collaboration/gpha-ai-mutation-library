2d0d77edd5e6a7a9f1bb19a342a84a9a65f2b04d
nfellaby <nfellaby@users.noreply.github.com>
2026-01-21 16:45:32 +0000

    feat/testing - updated fixture locations

--- tests/test_validate_and_update.py
+++ tests/test_validate_and_update.py
@@ -6,17 +6,17 @@ from mutation_table_updater import validate_and_update as vau
 import logging
 
 
-## Fixtures
+########################### Fixtures ###########################
 @pytest.fixture
 def correct_ha_tsv():
     SCRIPT_DIR = Path(__file__).resolve().parent
-    ha_tsv_fp = SCRIPT_DIR / "tables/ha_correct_test.tsv"
+    ha_tsv_fp = SCRIPT_DIR / "tables/correct_tables/ha_avian_influenza_mutation_table_gpha.tsv"
     return ha_tsv_fp
 
 @pytest.fixture
 def failed_fn_tsv():
     SCRIPT_DIR = Path(__file__).resolve().parent
-    failed_tsv_fp = SCRIPT_DIR / "tables/incorrect_fn_test.tsv"
+    failed_tsv_fp = SCRIPT_DIR / "tables/incorrect_tables/incorrect_fn_test.tsv"
     return failed_tsv_fp
 
 @pytest.fixture
@@ -39,21 +39,21 @@ def load_schemas():
 @pytest.fixture
 def load_bad_schemas():
     SCRIPT_DIR = Path(__file__).resolve().parent
-    schemas_dir = SCRIPT_DIR / "schemas/bad_top_key_schema/"
+    schemas_dir = SCRIPT_DIR / "schemas/bad_schemas/bad_top_key_schema/"
     schemas_map = vau.load_schemas(schemas_dir)
     return schemas_map
 
 @pytest.fixture
 def load_extra_colname_schemas():
     SCRIPT_DIR = Path(__file__).resolve().parent
-    schemas_dir = SCRIPT_DIR / "schemas/extra_columns_key_schema/"
+    schemas_dir = SCRIPT_DIR / "schemas/bad_schemas/extra_columns_key_schema/"
     schemas_map = vau.load_schemas(schemas_dir)
     return schemas_map
 
 @pytest.fixture
 def load_missing_colname_schemas():
     SCRIPT_DIR = Path(__file__).resolve().parent
-    schemas_dir = SCRIPT_DIR / "schemas/missing_columns_key_schema/"
+    schemas_dir = SCRIPT_DIR / "schemas/bad_schemas/missing_columns_key_schema/"
     schemas_map = vau.load_schemas(schemas_dir)
     return schemas_map
 
@@ -64,7 +64,16 @@ def load_good_schemas():
     schemas_map = vau.load_schemas(schemas_dir)
     return schemas_map
 
-## Tests
+@pytest.fixture
+def load_good_tables():
+    SCRIPT_DIR = Path(__file__).resolve().parent
+    schemas_dir = SCRIPT_DIR / "tables/correct_tables/"
+    schemas_map = vau.load_schemas(schemas_dir)
+    return schemas_map
+
+########################### Tests ###########################
+
+## Schema tests
 def test_find_schema_for_file(load_schemas, correct_ha_tsv):
     """ Test that the schema file is retrieved. """
     schema = vau.find_schema_for_file(load_schemas, correct_ha_tsv)
@@ -73,51 +82,6 @@ def test_find_schema_for_file(load_schemas, correct_ha_tsv):
 def test_validate_dataframe(correct_ha_df, correct_ha_schema):
     errs = vau.validate_dataframe(correct_ha_df, correct_ha_schema)
     assert errs == []
-
-def test_check_incorrect_filename(failed_fn_tsv, caplog):
-    """ Test to check incorrect filename are handled correctly"""
-    # Capture warnings
-    
-    caplog.set_level(logging.WARNING, logger=__name__)
-
-    segs = ['pb2','pb1','ha','m','na','np','ns','pa']
-
-    check_fn_return = vau.check_filename(failed_fn_tsv, segs)
-
-    # Expected warning message
-    warning_message = "Input File %s did not start with a segment ID (%s). Skipped." % (
-        Path(failed_fn_tsv).name, ", ".join(segs)
-    )
-
-    # # Assert warning was raised for inappropriate filename
-    assert any(
-            rec.levelno == logging.WARNING and str(warning_message) in rec.message
-            for rec in caplog.records
-        )
-
-    assert check_fn_return is False
-
-def test_check_correct_filename(correct_ha_tsv):
-    """ Test to check correct filename are handled correctly"""
-    # Capture warnings
-    
-    segs = ['pb2','pb1','ha','m','na','np','ns','pa']
-
-    check_fn_return = vau.check_filename(correct_ha_tsv, segs)
-
-    assert check_fn_return is True
-
-
-# def test_validate_schemas_correct(load_schemas, correct_ha_tsv):
-#     """ Test schema contains the right information """
-#     schemas_map  = vau.validate_schemas(load_schemas)
-
-#     # what fields are we expecting
-#     # keys: name, filname, strict_columns, primary_key, columns
-#     # Primary Key should container: "feature_type", "combination_name", "combination_id", "segment", "position_met1", "position_H5", "position_H3", "ref_AA", "alt_AA", "name", "subtype_tested", "subtype_notes", "phenotypic_consequences", "phenotypic_category", "concern_score", "confidence_score", "phenotypic_effect", "host_type", "host_taxon", "source", "references", "PMID", "additional_comments", "gpha_background", "gpha_known_muts"
-#     # columns, each item should contain: 'name', 'type', 'required'
-
-#     pass
 
 def test_incorrect_main_key_schema_file(load_bad_schemas, caplog):
     """ Test schema contains the right information """
@@ -157,7 +121,6 @@ def test_correct_main_key_schema_file(load_good_schemas):
     schema_val_status  = vau.validate_schemas(load_good_schemas)
     assert schema_val_status is True
 
-
 def test_find_schema_for_file(load_schemas, correct_ha_tsv):
     """ Test the correct schema is found for a file """
     sch  = vau.find_schema_for_file(load_schemas, correct_ha_tsv)
@@ -170,6 +133,50 @@ def test_incorrect_find_schema_for_file(load_schemas, correct_ha_tsv):
     
     assert sch['name'] not in ['pb2','pb1', 'm','na','np','ns','pa']
 
+def test_correct_map_schame_file(files, schemas):
+    """ Test mapping of schema to file """
+    # All segment files
+    # All segment schemas
+    # Result is dict with all files and schemas mapped and an empty skipped_files list
+    file_schema_map = vau.map_schemas_to_files(files, schemas)
+
+    for f in files:
+        assert f in file_schema_map
+        assert file_schema_map[f]['name'] in ['pb2','pb1','ha','m','na','np','ns','pa']
+
+## Test names of files
+def test_check_incorrect_filename(failed_fn_tsv, caplog):
+    """ Test to check incorrect filename are handled correctly"""
+    # Capture warnings
+    
+    caplog.set_level(logging.WARNING, logger=__name__)
+
+    segs = ['pb2','pb1','ha','m','na','np','ns','pa']
+
+    check_fn_return = vau.check_filename(failed_fn_tsv, segs)
+
+    # Expected warning message
+    warning_message = "Input File %s did not start with a segment ID (%s). Skipped." % (
+        Path(failed_fn_tsv).name, ", ".join(segs)
+    )
+
+    # # Assert warning was raised for inappropriate filename
+    assert any(
+            rec.levelno == logging.WARNING and str(warning_message) in rec.message
+            for rec in caplog.records
+        )
+
+    assert check_fn_return is False
+
+def test_check_correct_filename(correct_ha_tsv):
+    """ Test to check correct filename are handled correctly"""
+    # Capture warnings
+    
+    segs = ['pb2','pb1','ha','m','na','np','ns','pa']
+
+    check_fn_return = vau.check_filename(correct_ha_tsv, segs)
+
+    assert check_fn_return is True
 
 # Test dataframe validation
 """
